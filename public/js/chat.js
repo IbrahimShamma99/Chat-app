0ccd3bfbf7c0f18dcdca55dc6e35f401d4314784
Ibrahim shamma <i.abushammah@gmail.com>
2019-11-20 18:30:56 +0200

    Count Test

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,14 @@
-io()
+const socket = io()
+
+socket.on("countUpdated",(count) => {
+    
+    console.log("Count has been updated ",count)
+        
+})
+
+document.querySelector("#increment").addEventListener("click",() => {
+    console.log("Clicked")
+    socket.emit("increment")
+})
+
+
