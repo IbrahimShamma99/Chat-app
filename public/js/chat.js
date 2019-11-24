b5fe5fca757d0f26998bc14e7d8d530e88c9e586
Ibrahim shamma <i.abushammah@gmail.com>
2019-11-24 22:04:43 +0200

    Cleaner Code

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,19 +1,29 @@
 const socket = io()
 
+//Element 
+const $MessageForm = document.querySelector("#SendMessageForSever")
+const $Message = document.querySelector("input")
+const $MessageFormButton = document.querySelector("button")
+const $SendLocation = document.querySelector("#send-location")
+
 socket.on("WelcomingMessage",(WelcomingMessage) => {
     
     console.log(WelcomingMessage)
         
 })
 
-document.querySelector("#SendMessageForSever").addEventListener("submit",(pp) => {
+$MessageForm.addEventListener("submit",(pp) => {
     pp.preventDefault() //for Cancelling Event {No Response}
 
-    
+    $MessageFormButton.setAttribute('disabled','disabled') //Disable Button after it is being Clicked
+
     const messageFromClient = document.querySelector('input').value //Take The Value from input
     console.log("Clicked") //To Check if it is clicked
     
     socket.emit("SendMessageForSever",messageFromClient,()=> {
+    $MessageFormButton.removeAttribute('disabled')
+    $Message.value = ''
+    $Message.focus()
     console.log("message was delivered")
     }) //Open back Socket From Client to Server To transfer Data
 })
@@ -26,10 +36,12 @@ socket.on("Message", (p) =>{
     console.log(p)
 })
 
-document.querySelector("#send-location").addEventListener('click', () =>{
+$SendLocation.addEventListener('click', () =>{
     if (!navigator.geolocation){
         return alert("Please Provide Location")
     }
+    $SendLocation.setAttribute("disabled",'disabled')
+    console.log("Location has been shared")
 
     socket.on("Broadcast Location With Users", (p) =>{
         console.log(p)
@@ -39,6 +51,8 @@ document.querySelector("#send-location").addEventListener('click', () =>{
         console.log(position.coords.longitude)
         socket.emit("Broadcast Location",{'latitude':position.coords.latitude,
         'longitude':position.coords.longitude})
+        $SendLocation.removeAttribute('disabled')
+    
 
     })
    
