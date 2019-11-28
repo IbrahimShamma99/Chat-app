f0c704bff2c18a4f4439af6c502f12d222d44cb1
Ibrahim shamma <i.abushammah@gmail.com>
2019-11-28 13:45:49 +0200

    Updating the system overall

---
+++ src/routers/user.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const {routes} = require("../utils/routes")
+const User = require("../models/user")
+const router = new express.Router()
+const auth = require('../middleware/auth')
+
+router.post(routes.signup, async (req , res) => {
+    const user = new User(req.body)
+    
+    try {
+        await user.save()
+        // TODO sendWelcomeEmail(user.email, user.name)
+        const token = await user.generateAuthToken()
+        res.status(201).send({ user, token })
+    } catch (e) {
+        res.status(400).send(e)
+    }
+
+
+})
+
+
+module.exports = router
---
+++ src/utils/routes.js
@@ -0,0 +1,5 @@
+const routes = {
+    signup:"/user"
+}
+
+module.exports = {routes}
