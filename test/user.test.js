4d342609b5fccb4f0c82841e95683cf4a58a262f
Ibrahim shamma <i.abushammah@gmail.com>
2019-11-28 17:58:17 +0200

    Modifying last commit

--- test/user.test.js
+++ test/user.test.js
@@ -1,9 +1,9 @@
 const request = require("supertest")
 const app = require("../src/app")
-
+const {routes} = require("../src/utils/routes")
 
 test("assign new user" , async () => {
-    await request(app).post('/user').send({
+    await request(app).post(routes.signup).send({
         name:"Ibrahimjaws",
         email:"inininin@gmail.com",
         password:"macgeetheking"
